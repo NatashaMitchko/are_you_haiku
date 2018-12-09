28ad09157bb8349611cd77d50e85b25c9fd6b59e
Natasha Mitchko <natashamitchko@gmail.com>
2018-12-09 15:16:45 -0500

    some comments

--- are_you_haiku.py
+++ are_you_haiku.py
@@ -1,4 +1,16 @@
 import pyphen
 
-s = pyphen.Pyphen(lang='en')
-print(s.inserted('syllable'))
+s = pyphen.Pyphen(lang="en")
+
+def count(word):
+    """
+    Returns the syllable count for a word
+    """
+    return len(s.inserted(word).split("-"))
+
+print(count("elephant"))
+print(count("Madagascar"))
+
+# What makes a syllabe?
+# Every syllable must have a vowel, and every vowel makes a syllable. 
+# Exceptions: double vowels (aardvark, 3 vowels, 2 syllables)
